47d4312c4338a628507b69fb3ab112b39e062a31
TengNg <nguyentin148@gmail.com>
2023-09-01 17:09:11 +0700

    feat: clean up

--- server/ecommerce/src/main/java/com/ndt/configs/WebAppContextConfig.java
+++ server/ecommerce/src/main/java/com/ndt/configs/WebAppContextConfig.java
@@ -25,7 +25,6 @@ import org.springframework.web.servlet.view.JstlView;
     "com.ndt.repositories",
     "com.ndt.services"
 })
-@PropertySource("classpath:configs.properties")
 public class WebAppContextConfig implements WebMvcConfigurer {
 	@Autowired
 	private Environment env;
@@ -39,7 +38,7 @@ public class WebAppContextConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/resources/js/");
     }
-    
+
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver r = new InternalResourceViewResolver();
@@ -48,19 +47,6 @@ public class WebAppContextConfig implements WebMvcConfigurer {
         r.setSuffix(".jsp");
         return r;
     }
-
-	@Bean
-	public Cloudinary cloudinary() {
-		Cloudinary cloudinary = new Cloudinary(
-				ObjectUtils.asMap(
-						"cloud_name", this.env.getProperty("cloudinary.cloud_name"),
-						"api_key", this.env.getProperty("cloudinary.api_id"),
-						"api_secret", this.env.getProperty("cloudinary.api_secret"),
-						"secure", true
-				)
-		);
-		return cloudinary;
-	}
 
 	@Bean
 	public CommonsMultipartResolver multipartResolver() {
